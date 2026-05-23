f0a5f176319390b8eced12c73b2d057d2b943822
Arthur Hozanna <arthurhozana123@gmail.com>
2026-05-23 10:44:18 +0700

    feat: setup http bootstrap

--- internal/bootstrap/http.go
+++ internal/bootstrap/http.go
@@ -1,1 +1,16 @@
 package bootstrap
+
+import (
+	"net/http"
+
+	"github.com/arthurhzna/go-clean-architecture/internal/config"
+	"github.com/gin-gonic/gin"
+)
+
+func NewHTTPServer(cfg *config.Config) *http.Server {
+	appCfg := cfg.App
+	gin.SetMode(appCfg.Environment)
+	router := gin.New()
+	router.ContextWithFallback = true
+	router.HandleMethodNotAllowed = true
+}
